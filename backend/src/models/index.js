7993d8e9c7d0e74dd94838ed7d258e8b4a080d83
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-05-19 19:19:48 +0200

    Created index.js for the models folder

---
+++ backend/src/models/index.js
@@ -0,0 +1,7 @@
+// Model imports
+const ChatModel = require('./chat');
+const MessageModel = require('./message');
+const UserModel = require('./user');
+
+// Export the models
+module.exports = { ChatModel, MessageModel, UserModel };
