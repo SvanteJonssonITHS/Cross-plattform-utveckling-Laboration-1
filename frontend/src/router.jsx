a18cb83c87d9294771294eb9822b1a1a75c279b2
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-05-23 15:42:25 +0200

    Added auth check to router

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,16 +1,22 @@
 // External dependencies
 import { StrictMode } from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 // Internal dependencies
 import Home from './views/Home';
+
+const authenticated = async () => {
+	const request = await fetch('/api/');
+	const response = await request.json();
+	return response.authenticated;
+};
 
 export default function () {
 	return (
 		<StrictMode>
 			<BrowserRouter>
 				<Routes>
-					<Route path="/" element={<Home />} />
+					<Route index element={authenticated() ? <Home /> : <Navigate to="/tjotahejti" />} />
 				</Routes>
 			</BrowserRouter>
 		</StrictMode>
