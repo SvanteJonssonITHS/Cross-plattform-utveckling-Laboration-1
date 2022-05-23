b1cf363f863ffdc1b472b6ae1143829816281cea
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-05-23 17:56:19 +0200

    Update LandingPage.jsx

--- frontend/src/views/LandingPage.jsx
+++ frontend/src/views/LandingPage.jsx
@@ -1,3 +1,47 @@
+import { Link, useNavigate } from 'react-router-dom';
+
 export default function () {
-	return <h1 className="text-3xl font-semibold">LandingPage</h1>;
+	const navigate = useNavigate();
+
+	return (
+		<main className="w-screen min-h-screen flex bg-gray-900 text-white">
+			<section className="w-1/2 flex flex-col justify-center items-center">
+				<div>
+					<h1 className="text-5xl font-bold mb-2">Texting has never been easier!</h1>
+					<p className="text-2xl text-white/[.75] mb-2">One-on-one or groups, we&#39;ve got you covered!</p>
+					<form
+						className="mb-1"
+						onSubmit={(event) => {
+							event.preventDefault();
+
+							const redirect = event.target.email.value
+								? `/register?email=${event.target.email.value}`
+								: '/register';
+
+							navigate(redirect);
+						}}
+					>
+						<input
+							name="email"
+							type="email"
+							placeholder="Email address"
+							className="pl-2 pr-10 py-2 rounded-md mr-2 text-black outline-none"
+						/>
+						<button className="px-7 py-2 rounded-md bg-green-500 font-semibold outline-none">
+							Sign Up
+						</button>
+					</form>
+					<span>
+						<p>
+							Already have an account?{' '}
+							<Link to="/login" className="text-green-500 font-semibold hover:underline ">
+								Login here!
+							</Link>
+						</p>
+					</span>
+				</div>
+			</section>
+			<section className="w-1/2">Image here</section>
+		</main>
+	);
 }
