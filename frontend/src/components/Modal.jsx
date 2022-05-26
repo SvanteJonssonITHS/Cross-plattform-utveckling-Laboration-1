e31176e004bfc6bb98b739a971f34b3d28e2ef32
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-05-26 21:06:56 +0200

    Update Modal.jsx

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -8,14 +8,14 @@ const closeModal = (event, onClose) => {
 
 export default (prop) => {
 	useEffect(() => {
-		if (typeof prop.onClose !== 'function') throw new Error('Prop onClose must be a function');
+		if (typeof prop.onClose !== 'function') throw new Error('Prop onClose must be a function (Modal.jsx)');
 	}, []);
 
 	if (!prop.isOpen) return null;
 
 	return (
 		<div
-			className="absolute w-screen h-screen bg-black/[.5] grid items-center"
+			className="absolute w-screen h-screen bg-black/[.5] grid items-center overflow-y-scroll"
 			onClick={(event) => closeModal(event, prop.onClose)}
 		>
 			{prop.children}
