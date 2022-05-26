6542e0b40d938c7818b76fb2ecc1ede0504eed86
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-05-26 18:51:14 +0200

    Created `Modal.jsx`

---
+++ frontend/src/components/Modal.jsx
@@ -0,0 +1,24 @@
+import { useEffect } from 'react';
+
+const closeModal = (event, onClose) => {
+	if (event.target === event.currentTarget) {
+		onClose();
+	}
+};
+
+export default (prop) => {
+	useEffect(() => {
+		if (typeof prop.onClose !== 'function') throw new Error('Prop onClose must be a function');
+	}, []);
+
+	if (!prop.isOpen) return null;
+
+	return (
+		<div
+			className="absolute w-screen h-screen bg-black/[.5] grid items-center"
+			onClick={(event) => closeModal(event, prop.onClose)}
+		>
+			{prop.children}
+		</div>
+	);
+};
