40d8dbf2039c08acd6ec0a4d9e0af96a18e51b94
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-05-26 14:23:15 +0200

    Created `UserContext.jsx`

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -4,23 +4,34 @@ import { Switch, Route, useLocation } from 'wouter';
 
 // Internal dependencies
 import { Home, LandingPage, Register, Login, PageNotFound } from './views';
+import { UserContext } from './contexts';
 
 export default function () {
 	const [location] = useLocation();
 	const [authenticated, setAuthenticated] = useState(false);
+	const [userId, setUserId] = useState(null);
 
 	useEffect(() => {
 		(async () => {
 			const request = await fetch('/api/');
 			const response = await request.json();
 			setAuthenticated(response.authenticated);
+			setUserId(response.userId);
 		})();
 	}, [location]);
 
 	return (
 		<StrictMode>
 			<Switch>
-				<Route path="/">{authenticated ? <Home /> : <LandingPage />}</Route>
+				<Route path="/">
+					{authenticated ? (
+						<UserContext.Provider value={{ userId }}>
+							<Home />
+						</UserContext.Provider>
+					) : (
+						<LandingPage />
+					)}
+				</Route>
 				{!authenticated && (
 					<Route path="/register">
 						<Register />
