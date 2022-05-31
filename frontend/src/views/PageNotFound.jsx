0bdf4ab538b73b67a4fb640db0726b7cb69e6679
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-05-31 19:01:40 +0200

    Update PageNotFound.jsx

--- frontend/src/views/PageNotFound.jsx
+++ frontend/src/views/PageNotFound.jsx
@@ -5,10 +5,10 @@ export default function () {
 		<main className="flex flex min-h-screen w-screen flex-col items-center justify-center bg-gray-900 text-white">
 			<h2 className="mb-5 text-7xl">404</h2>
 			<h3 className="mb-5 text-4xl">Page not found</h3>
-			<p className="text-lg">
+			<p className="text-center text-lg">
 				Sorry but the page you are looking for does not exist. To go back to the homepage,{' '}
 				<Link href="/">
-					<a className="font-semibold text-green-500 hover:underline">click here</a>
+					<a className="font-semibold text-green-500 hover:underline">click&nbsp;here</a>
 				</Link>
 			</p>
 		</main>
